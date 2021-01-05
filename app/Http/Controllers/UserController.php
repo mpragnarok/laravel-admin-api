e83bdd81c75e9aaea2f4d4920949f3496f2fc808
mina.h <mina.h@amaryllo.us>
2021-01-05 16:07:16 +0800

    <add><CRUD user>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,62 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return User[]
      */
     public function index()
     {
-        //
+        return User::all();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email')
+            // + to merge the array
+            + ['password' => Hash::make(1234)]);
+        return response($user, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return User
      */
     public function show($id)
     {
-        //
+        return User::find($id);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        $user->update($request->only('first_name', 'last_name', 'email'));
+        return response($user, Response::HTTP_ACCEPTED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        User::destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
+
     }
 }
