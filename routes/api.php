bca7cd515b171f450f88b4530a7c9051d89fd2ac
mina.h <mina.h@amaryllo.us>
2021-01-05 15:51:07 +0800

    <add><apiResource>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +22,8 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+
+    Route::apiResource('users',UserController::class);
 
 });
