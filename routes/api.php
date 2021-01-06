a2f62ebc7b8d1ca43cfad64ad0c5f5ad85d80b25
mina.h <mina.h@amaryllo.us>
2021-01-06 15:40:49 +0800

    <add><model and migrate><role and permission>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,5 +29,7 @@ Route::middleware('auth:sanctum')->group(function (){
 
 
     Route::apiResource('users',UserController::class);
+    Route::apiResource('roles',RoleController::class);
+    Route::get('permissions',[PermissionController::class, 'index']);
 
 });
