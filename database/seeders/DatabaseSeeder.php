017744436464f60159d3e3cf1496641099f38c70
mina.h <mina.h@amaryllo.us>
2021-01-07 14:34:53 +0800

    <add><create role foreign keys in user>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(20)->create();
+        // seed role
+        Role::factory()->create(['name'=>'Admin']);
+        Role::factory()->create(['name'=>'Editor']);
+        Role::factory()->create(['name'=>'Viewer']);
+        // seed user
+        \App\Models\User::factory(20)->create();
+        \App\Models\User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'role_id' => '1',
+        ]);
+        \App\Models\User::factory()->create([
+            'first_name' => 'Editor',
+            'last_name' => 'Editor',
+            'email' => 'editor@editor.com',
+            'role_id' => '2',
+        ]);
+        \App\Models\User::factory()->create([
+            'first_name' => 'Viewer',
+            'last_name' => 'Viewer',
+            'email' => 'viewer@viewer.com',
+            'role_id' => '3',
+        ]);
     }
 }
